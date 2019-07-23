fc68f040c6e0aab42022a01da6551b7c45a5da9a
Javier9818 <53130037+Javier9818@users.noreply.github.com>
2019-07-23 08:07:08 -0500

    Update PruebasCumplimientoR.php

--- VerAuditoria/PruebasCumplimientoR.php
+++ VerAuditoria/PruebasCumplimientoR.php
@@ -1,10 +1,10 @@
-<h1 class="text-center card-header">Pruebas de Cumplimiento</h1>
+<h1 class="text-center card-header">Pruebas de Cumplimiento Ejecutadas</h1>
 <br>
 
 <script>
-	function Pasar(id){
+	function PasarR(id){
 			document.getElementById("parametro").value=id;
-			document.getElementById("MostrarPrueba").click();
+			document.getElementById("MostrarPruebaR").click();
 	}
 </script>
 
@@ -13,18 +13,28 @@
  	session_start();
  	$id = $_SESSION['id'];	
 
- 	$sql = "SELECT * FROM pruebaCumplimiento WHERE IdAuditoria='".$id."' order by IdPrueba desc";
+ 	$sql = "SELECT * FROM PruebaCumplimientoRealizada pr JOIN PruebaCumplimiento pc ON pr.IdPrueba=pc.IdPrueba WHERE pc.IdAuditoria='".$id."' ORDER BY  pr.FechaEjecucion DESC";
 	$sql=$conexion->query($sql);
+
+	?>
+		<div class="row  grey text-white">
+   				<div class="col-md-3"><h6>Fecha de Ejecución</h6></div>
+				<div class="col-md-4"><h6>Nombre de Prueba</h6></div>
+				<div class="col-md-3"><h6>Auditado</h6></div>
+		</div>
+		<br>
+	<?php
 
 	 while($row=mysqli_fetch_array($sql))
 	 	{
    			?>
 
+
    			<div class="row justify-content-around">
-   				<div class="col-md-3"><h6><?php echo $row['FechaRegistro']  ?></h6></div>
+   				<div class="col-md-3"><h6><?php echo $row['FechaEjecucion']  ?></h6></div>
 				<div class="col-md-4"><h6><?php echo $row['Nombre'] ?></h6></div>
-				<div class="col-md-3"><h6>Briceño Montaño Javier</h6></div>
-				<div class="col-md-2"><a class="btn btn-blue btn-block animated fadeInRight" onclick="Pasar(<?php echo $row['IdPrueba'] ?>)" data-toggle="modal" data-target="#DocInv"><i class="fas fa-eye fa-2x "></i><div>Mostrar</div></a></div>
+				<div class="col-md-3"><h6><?php echo $row['Auditado'] ?></h6></div>
+				<div class="col-md-2"><a class="btn btn-blue btn-block animated fadeInRight" onclick="PasarR(<?php echo $row['IdPruebaRealizada'] ?>)" data-toggle="modal" data-target="#DocInv"><i class="fas fa-eye fa-2x "></i><div>Mostrar</div></a></div>
 			</div>
 
 			<hr>
