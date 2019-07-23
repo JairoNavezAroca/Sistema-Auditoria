3b800cd154ac8691b09cca6982d364af710ae912
Javier9818 <53130037+Javier9818@users.noreply.github.com>
2019-07-23 08:06:44 -0500

    Update GuardarPruebaSustantiva.php

--- VerAuditoria/GuardarPruebaSustantiva.php
+++ VerAuditoria/GuardarPruebaSustantiva.php
@@ -10,14 +10,15 @@
 			$Fecha=$_POST["Fecha"];
 			$Institucion=$_POST["Institucion"];
 			$IdPrueba=$_POST["IdPrueba"];
+			$Respuestas=$_POST["Respuestas"];
 
 			$NormasG=$_POST["NG"];
 			$PorcG=$_POST["PORC"];
 
-			echo $NormasG." ".$PorcG;
+			
 
 			include("..//conexion.php");
-			$sql = "INSERT INTO PruebaCumplimientoRealizada(Auditado,FechaEjecucion,institucion,IdPrueba,ConteoGeneral,ConteoIndividual,Normas,PorcG) VALUES('".$Auditado."','".$Fecha."','".$Institucion."','".$IdPrueba."','".$ConteoGeneral."','".$ConteoIndividual."','".$NormasG."','".$PorcG."')";
+			$sql = "INSERT INTO PruebaCumplimientoRealizada(Auditado,FechaEjecucion,institucion,IdPrueba,ConteoGeneral,ConteoIndividual,Normas,PorcG,Respuestas) VALUES('".$Auditado."','".$Fecha."','".$Institucion."','".$IdPrueba."','".$ConteoGeneral."','".$ConteoIndividual."','".$NormasG."','".$PorcG."','".$Respuestas."')";
 			if($conexion->query($sql))
 			{
 				?> <h5>Registro Exitoso!</h5> <?php
